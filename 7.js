59c0b7daf3a6b9f829a6fc1394172cd2a5d9b841
Aminuddin77 <aminzainuddin22@gmail.com>
2023-09-01 18:14:59 +0500

    Js first Assignment

---
+++ 7.js
@@ -0,0 +1,64 @@
+// Answer#1
+
+alert("Welcome")
+
+// ANSWER #2
+alert("Error! Please enter a valid password.")
+
+// ANSWER # 3
+alert("Welcome to JS Land..\nHappy Coding!")
+
+// ANSWER# 4
+alert("Welcome to JS Land...")
+alert("Happy coding!\n prevent this page from creating additional dialog")
+
+// ANSWER# 5
+let text;
+  let text2 = prompt("Please enter your message:");
+  alert(text2)
+
+
+
+
+// ASSIGNMENT FOR CHAPTER 2
+
+// ANSWER # 1
+var name="AMEEN"
+
+// ANSWER#2
+var name2="Zain"
+console.log( name + name2)
+
+// ANSWER#3 
+var message;
+message="Hello World";
+alert(message);
+
+// ANSWER# 4
+var fullname="Ameen Zain";
+alert(fullname);
+
+var age=21;
+alert(age);
+
+var job="Certified Mobile Application Development";
+alert(job);
+
+// ANSWER# 5
+var pizza="pizza \npizz \npiz \npi \np";
+alert(pizza);
+    
+// ANSWER# 6
+var email="aminzainuddin12@gmail.com"
+alert("My email address is "+email)
+
+// ANSWER# 7
+var book="I Am Trying To Learn From Smarter Way To Learn JS"
+alert(book)
+
+// ANSWER# 8  
+document.writeln("Yah! I can write HTML content through JavaScript");
+
+// ANSWER# 9
+var string="“▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬”"
+alert(string);
